5375ba70571ba5313748c81beba5fea896b5e098
alexchekmenev <alexchekmenev@mail.ru>
2017-04-11 05:00:22 +0300

    almost done

--- app/Main.hs
+++ app/Main.hs
@@ -3,28 +3,33 @@
 
 module Main where
 
-import           Control.Concurrent                   (forkIO, threadDelay)
-import           Control.Monad
-import           Control.Monad.IO.Class               (liftIO)
-import           Control.Monad.Trans.Class
-import           Control.Monad.Trans.Maybe
-import           Dao
-import           Data.Acid                            (openLocalState, query,
-                                                       update)
-import           Data.Acid.Abstract                   (AcidState)
-import           Data.IntMap                          (empty)
-import           Data.Text.Lazy                       (pack)
-import           Data.Time.Clock                      (getCurrentTime)
-import           Data.Time.Format
+import           Control.Concurrent        (forkIO, threadDelay)
+import           Control.Monad.IO.Class    (liftIO)
+import           Control.Monad.Trans.Class (lift)
+import           Data.Acid                 (openLocalState, query, update)
+import           Data.Acid.Abstract        (AcidState)
+import           Data.IntMap               (empty)
+import           Data.Text.Lazy            (Text, pack, unpack)
+import           Data.Time.Clock           (getCurrentTime)
 import           Model
-import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import qualified Web.Scotty                           as W
+-- import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import qualified Web.Scotty                as W
 
+{-|
+  Helper function is wrapper for threadDelay
+-}
+sleepMs :: Int -> IO ()
 sleepMs n = threadDelay (n * 1000)
 
+{-|
+  Print Message to stdin
+-}
 printMessage :: Message -> IO ()
-printMessage m = putStrLn $ "["++ sender m ++ "]: " ++ text m
+printMessage m = putStrLn $ "["++ unpack (sender m) ++ "]: " ++ unpack (text m)
 
+{-|
+  Function is the implementation of the Finite-state machine of the client env
+-}
 client :: AcidState MessagesDb -> AcidState ChatContextDb -> IO ()
 client msgDb ctxDb = do
   ctx <- getContext ctxDb
@@ -32,79 +37,94 @@ client msgDb ctxDb = do
   if login ctx == "" && activeChat ctx == "" then do
     putStrLn "Enter your login or `:q` to quit"
     ln <- getLine
-    when (ln /= ":q") (do
+    if ln /= ":q" then do
       putStrLn $ "Your login: `" ++ ln ++ "`"
-      updateContext ctxDb $ ChatContext ln ""
-      client msgDb ctxDb)
-    when (ln == ":q") (do
+      updateContext ctxDb $ ChatContext (pack ln) ""
+      client msgDb ctxDb
+    else do
       putStrLn "Bye! Press ^C to exit"
-      return ())
+      return ()
   else
     if activeChat ctx == "" then do
       putStrLn "Enter your recepient login or `:q` to choose new login"
       rc <- getLine
-      when (rc /= ":q") (do
+      if rc /= ":q" then do
         putStrLn $ "Your chat with `" ++ rc ++ "` (`:q` to choose another chat)"
         putStrLn "..."
-        msgs <- lastKMessages msgDb rc 10
+        msgs <- lastKMessages msgDb (pack rc) 10
         mapM_ printMessage msgs
-        updateContext ctxDb $ ChatContext (login ctx) rc
-        client msgDb ctxDb)
-      when (rc == ":q") (do
+        updateContext ctxDb $ ChatContext (login ctx) (pack rc)
+        client msgDb ctxDb
+      else do
         putStrLn "You are logged out"
         updateContext ctxDb $ ChatContext "" ""
-        client msgDb ctxDb)
+        client msgDb ctxDb
     else do
       msg <- getLine
-      when (msg /= ":q") (do
+      if msg /= ":q" then do
         timestamp <- getCurrentTime
-        void $ saveMessage msgDb $ Message (login ctx) (activeChat ctx) msg timestamp
-        putStrLn $ "["++ login ctx ++"]: " ++ msg
-        client msgDb ctxDb)
-      when (msg == ":q") (do
+        saveMessage msgDb $ Message (login ctx) (activeChat ctx) (pack msg) timestamp
+        putStrLn $ "["++ unpack (login ctx) ++"]: " ++ msg
+        client msgDb ctxDb
+      else do
         updateContext ctxDb $ ChatContext (login ctx) ""
-        client msgDb ctxDb)
+        client msgDb ctxDb
 
+{-|
+  Main Scotty server function
+-}
 main :: IO ()
 main = do
   state <- openLocalState (MessagesDb empty)
-  chatContext <- openLocalState (ChatContextDb $ ChatContext "" "")
+  ctxDb <- openLocalState (ChatContextDb $ ChatContext "" "")
   -- updateContext chatContext $ ChatContext "" ""
-  forkIO (do
+  _ <- forkIO $ do
     sleepMs 3
-    client state chatContext)
+    client state ctxDb
   W.scotty 3000 $ do
     -- middleware logStdoutDev
     W.get "/" $ do
-      a <- liftIO $ gallMessages state
+      a <- liftIO $ getAllMessages state
       W.text $ pack $ show a
     W.get "/receive/:from/:text" $ do
-      msgFrom :: String <- W.param "from"
-      msgText :: String <- W.param "text"
+      msgFrom :: Text <- W.param "from"
+      msgText :: Text <- W.param "text"
       timestamp <- lift getCurrentTime
-      c <- liftIO $ getContext chatContext
+      c <- liftIO $ getContext ctxDb
       liftIO $ saveMessage state $ Message msgFrom (login c) msgText timestamp
       if msgFrom == activeChat c && msgFrom /= ""
         then do
-          lift $ putStrLn $ "[" ++ msgFrom ++ "]: " ++ msgText
+          lift $ putStrLn $ "[" ++ unpack msgFrom ++ "]: " ++ unpack msgText
           W.text "Ok"
         else
           W.text "Ok"
--- sendMessage :: AcidState MessagesDb -> String -> IO ()
 
+{-|
+  Gets context from its acid-state
+-}
 getContext :: AcidState ChatContextDb -> IO ChatContext
 getContext state = query state GetChatContext
 
-updateContext :: AcidState ChatContextDb -> ChatContext -> IO ChatContext
-updateContext state newCtx = do
-  void $ update state $ UpdateChatContext newCtx
-  return newCtx
+{-|
+  Updates context stored in its acid-state
+-}
+updateContext :: AcidState ChatContextDb -> ChatContext -> IO ()
+updateContext state newCtx = update state $ UpdateChatContext newCtx
 
+{-|
+  Saves Message to its acid-state
+-}
 saveMessage :: AcidState MessagesDb -> Message -> IO ()
 saveMessage state msg = update state $ AddMessage msg
 
-gallMessages :: AcidState MessagesDb -> IO [Message]
-gallMessages state = query state GetMessages
+{-|
+  Gets all stored messages
+-}
+getAllMessages :: AcidState MessagesDb -> IO [Message]
+getAllMessages state = query state GetMessages
 
-lastKMessages :: AcidState MessagesDb -> String -> Int -> IO [Message]
+{-|
+  Gets from acid-state last k messages from the dialog with to
+-}
+lastKMessages :: AcidState MessagesDb -> Text -> Int -> IO [Message]
 lastKMessages state to k = query state $ LastKChatMessages to k
--- src/Model.hs
+++ src/Model.hs
@@ -1,23 +1,107 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE InstanceSigs          #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+
 module Model where
 
-import qualified Data.IntMap     as IntMap
-import           Data.Time.Clock
-import           Data.Typeable
+import           Control.Monad.Reader (ask)
+import           Control.Monad.State  (modify)
+import           Data.Acid            (Query, Update, makeAcidic)
+import           Data.IntMap          (IntMap, elems, insert, maxViewWithKey,
+                                       singleton)
+import           Data.List            (sortBy)
+import           Data.Ord             (comparing)
+import           Data.SafeCopy        (base, deriveSafeCopy)
+import           Data.Text.Lazy       (Text)
+import           Data.Time.Clock      (UTCTime)
+import           Data.Typeable        (Typeable)
+import           Prelude              hiding (max)
 
-data Message = Message {
-  sender    :: String,
-  recepient :: String,
-  text      :: String,
-  time      :: UTCTime
+{-|
+  Represents the Message entity
+-}
+data Message =
+  Message { -- | sender login
+            sender    :: Text,
+            -- | recepient login
+            recepient :: Text,
+            -- | text of the Message
+            text      :: Text,
+            -- | time of receipt
+            time      :: UTCTime
 } deriving (Show, Typeable)
 
-data MessagesDb = MessagesDb { allMessages :: IntMap.IntMap Message }
+{-|
+  Storage for messages
+-}
+newtype MessagesDb = MessagesDb { allMessages :: IntMap Message }
   deriving (Typeable)
 
-data ChatContext = ChatContext {
-  login      :: String,
-  activeChat :: String
+{-|
+  Represents context of the chat ui
+-}
+data ChatContext =
+  ChatContext { -- | login of the current sender
+                login      :: Text,
+                -- | login of the current recepient
+                activeChat :: Text
 } deriving (Show, Typeable)
 
-data ChatContextDb = ChatContextDb { chatContext :: ChatContext }
+{-|
+  Storage for the ui context
+-}
+newtype ChatContextDb = ChatContextDb { chatContext :: ChatContext }
   deriving (Typeable)
+
+deriveSafeCopy 0 'base ''Message
+deriveSafeCopy 0 'base ''MessagesDb
+deriveSafeCopy 0 'base ''ChatContext
+deriveSafeCopy 0 'base ''ChatContextDb
+
+{-|
+  Gets all messages from its storage
+-}
+getMessages :: Query MessagesDb [Message]
+getMessages = sortBy (comparing time) . elems . allMessages <$> ask
+
+{-|
+  Gets lask k messages from the curtain dialog
+-}
+lastKChatMessages :: Text -> Int -> Query MessagesDb [Message]
+lastKChatMessages to k =
+  reverse .
+  take k .
+  sortBy (flip (comparing time)) .
+  filter (\m -> recepient m == to || sender m == to) .
+  elems . allMessages <$> ask
+
+{-|
+  Puts new message to the storage
+-}
+addMessage :: Message -> Update MessagesDb ()
+addMessage message = modify go
+  where
+    go (MessagesDb db) = MessagesDb $
+      case maxViewWithKey db of
+        Just ((max, _), _) -> insert (max + 1) message db
+        Nothing            -> singleton 1 message
+
+makeAcidic ''MessagesDb ['addMessage, 'getMessages, 'lastKChatMessages]
+
+{-|
+  Gets the chat context from its storage
+-}
+getChatContext :: Query ChatContextDb ChatContext
+getChatContext = chatContext <$> ask
+
+{-|
+  Updates the chat context & saves it to its storage
+-}
+updateChatContext :: ChatContext -> Update ChatContextDb ()
+updateChatContext ctx = modify go
+  where
+    go _ = ChatContextDb $ ChatContext (login ctx) (activeChat ctx)
+
+makeAcidic ''ChatContextDb ['getChatContext, 'updateChatContext]
